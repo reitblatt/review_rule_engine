c2ff4baa6e0241d2ce19936b6a99674b83fa127f
Mark Reitblatt <mark@reitblatt.com>
2022-10-31 20:55:15 -0700

    Move register() logic from rule_engine to Trigger as intended

--- reviews/models.py
+++ reviews/models.py
@@ -6,53 +6,56 @@ from . import signals
     To demonstrate the rule engine, we build a simple review system over a basic 'post' object.
 """
 
+
 class Post(models.Model):
     class State(models.TextChoices):
         PUBLISHED = 'published'
         DELETED = 'deleted'
-        
+
     state = models.CharField(
         max_length=50,
         choices=State.choices
     )
-    
+
     def __str__(self):
         return f"{self.pk} ({self.state})"
+
 
 class PostReviewJob(models.Model):
     class State(models.TextChoices):
         UNREVIEWED = 'unreviewed'
         IN_PROGRESS = 'in_progress'
         CLOSED = 'closed'
-        
+
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=50,
         choices=State.choices
     )
-    
+
     def __str__(self):
         return f"{self.post} - {self.status}"
 
 
 class Reviewer(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class PostReview(models.Model):
     class Decision(models.TextChoices):
         IGNORE = 'ignore'
         DELETE = 'delete'
-        
+
     reviewer = models.ForeignKey(
-        Reviewer, 
+        Reviewer,
         on_delete=models.CASCADE,
         related_name='reviews'
     )
     job = models.ForeignKey(
-        PostReviewJob, 
+        PostReviewJob,
         on_delete=models.CASCADE,
         related_name='reviews'
     )
@@ -60,80 +63,88 @@ class PostReview(models.Model):
         max_length=50,
         choices=Decision.choices,
     )
-    
+
     def __str__(self):
         return f"job {self.job.pk} - {self.reviewer} - {self.decision}"
-    
+
+
 """
     Set up rule engine to run over the review objects
 """
 
 RuleTarget = PostReview
 
+
 class Trigger(rule_models.Trigger):
     class Choice(models.TextChoices):
         ON_REVIEW_CREATE = 'on_review_create'
-        
+
     trigger_name = models.CharField(
         max_length=200,
         choices=Choice.choices
     )
     sender = models.CharField(max_length=200)
-    
-    def get_signal(self):
+
+    def register(self, callback):
+        signal = self._get_signal()
+        signal.connect(lambda instance, **
+                       kwargs: callback(target=instance), weak=False)
+
+    def _get_signal(self):
         match self.trigger_name:
             case __class__.Choice.ON_REVIEW_CREATE:
                 print("matched CREATE trigger")
                 return signals.review_completed
             case default:
                 print(f"unknown trigger {self.trigger_name}")
-    
+
     def __str__(self):
         return f"{self.trigger_name} ON {self.sender}"
-    
+
+
 class Condition(rule_models.Condition):
     class Choices(models.TextChoices):
         IS_NTH_REVIEW = 'is_nth_review'
         DECISION_IS = 'decision_is'
         ALL_DECISIONS_MATCH = 'all_decisions_match'
-        
+
     property = models.CharField(
         max_length=100,
-        choices = Choices.choices,
+        choices=Choices.choices,
     )
-    
+
     # Very simple model, not forcing the value to match with the property...
     value = models.CharField(
         max_length=100,
         null=True,
     )
-    
+
     def is_satisfied(self, target: PostReview):
         match self.property:
             case __class__.Choices.IS_NTH_REVIEW:
-                n = int(self.value)                
+                n = int(self.value)
                 return n == target.job.reviews.count()
             case __class__.Choices.DECISION_IS:
                 decision = PostReview.Decision(self.value)
                 return target.decision == decision
             case __class__.Choices.ALL_DECISIONS_MATCH:
                 return 1 >= target.job.reviews.values('decision').distinct().count()
-            
+
     def __str__(self):
         return f"{self.property}: {self.value}"
-                
-    
+
+
 class Effect(rule_models.Effect):
     class Choices(models.TextChoices):
         CLOSE = "close_job"
         DELETE_AND_CLOSE = 'delete_and_close'
         NOTHING = 'nothing'
-        
+
     name = models.CharField(
         max_length=50,
-        choices = Choices.choices
+        choices=Choices.choices
     )
-    
+
     def perform_effect(self, target: PostReview):
         match self.name:
             case __class__.Choices.CLOSE:
@@ -147,10 +158,11 @@ class Effect(rule_models.Effect):
                 target.job.post.save()
             case __class__.Choices.NOTHING:
                 pass
-            
+
     def __str__(self):
         return self.name
-            
+
+
 class ReviewRule(rule_models.Rule):
     def __str__(self):
         return f"WHENEVER [{self.trigger}] IF [{self.condition}] DO [{self.effect}]"
--- rule_engine/rule_engine.py
+++ rule_engine/rule_engine.py
@@ -1,15 +1,17 @@
 from django.db.models.signals import post_save
 from . import models
 
-    
+
 """
     Todo: make this a singleton class
 """
+
+
 class RuleEngine:
     __known_triggers = None
     __rule_class = None
     __trigger_class = None
-    
+
     def __init__(self, RuleClass: type[models.Rule], TriggerClass: type[models.Trigger]):
         """
             Note: we have to do the group by list in python because django doesn't provide a list aggregation. 
@@ -18,33 +20,30 @@ class RuleEngine:
         self.__rule_class = RuleClass
         self.__trigger_class = TriggerClass
         print(self.__trigger_class)
-        self.__known_triggers = {value['trigger'] for value in RuleClass.objects.values('trigger').distinct()}
-        triggers = [TriggerClass.objects.get(pk=trigger_pk) for trigger_pk in self.__known_triggers]
-        print(triggers)        
-        for trigger in triggers:            
+        self.__known_triggers = {
+            value['trigger'] for value in RuleClass.objects.values('trigger').distinct()}
+        triggers = [TriggerClass.objects.get(
+            pk=trigger_pk) for trigger_pk in self.__known_triggers]
+        print(triggers)
+        for trigger in triggers:
             self.__register_trigger(trigger)
-        post_save.connect(self.register_new_rule, sender=models.Rule, weak=False)
-        
-    def __handle_trigger(self, trigger: models.Trigger, sender, instance, **kwargs):
-        print(sender)
-        print(instance)
+        post_save.connect(self.register_new_rule,
+                          sender=models.Rule, weak=False)
+
+    def __handle_trigger(self, trigger: models.Trigger, target):
+        print(target)
         for rule in self.__rule_class.objects.filter(trigger=trigger):
             print(f"about to run {rule}")
-            rule.run_rule(instance)
+            rule.run_rule(target)
             print(f"just ran rule {rule}")
-    
+
     def __register_trigger(self, trigger: models.Trigger):
         print(f"registering trigger {trigger}")
         # todo: get the sender from the trigger itself
-        trigger.get_signal().connect(
-            lambda sender, instance, **kwargs: self.__handle_trigger(trigger, sender, instance, **kwargs),
-            weak=False
-        )
-        
-    def register_new_rule(self, rule: models.Rule, **kwargs):                
+        trigger.register(lambda target:
+                         self.__handle_trigger(trigger, target))
+
+    def register_new_rule(self, rule: models.Rule, **kwargs):
         if rule.trigger not in self.__known_triggers:
-            self.__register_trigger(rule.trigger)            
+            self.__register_trigger(rule.trigger)
         self.__known_triggers += rule.trigger
-
-    
-
