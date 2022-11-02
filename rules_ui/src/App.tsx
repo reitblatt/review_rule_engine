0b8ed917acbbc1ce1aa0ef5a303d8d9398b30412
Mark Reitblatt <mark@reitblatt.com>
2022-11-02 11:39:03 -0700

    Moved table + query to separate component

--- rules_ui/src/App.tsx
+++ rules_ui/src/App.tsx
@@ -1,23 +1,12 @@
 import './App.css';
-import ReviewRule from './rules_ui/RulesList';
-import { useQuery } from '@apollo/client'
-import { graphql } from '../src/gql'
+import ReviewRuleTable from './rules_ui/RulesList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const allReviewRulesQueryDocument = graphql(/* GraphQL */ `
-  query allReviewRulesQuery {
-    rules {
-      id      
-      ...ReviewRuleItem  
-    }
-  }
-`)
 
 function App() {
-  const { data } = useQuery(allReviewRulesQueryDocument)
   return (
-    <div className="App">
-      {data && <ul>{data.rules?.map(e => e && <ReviewRule rule={e} key={e.id} />)}</ul>}
+    <div className='App'>
+      <ReviewRuleTable />
     </div>
   )
 }
--- rules_ui/src/rules_ui/RulesList.tsx
+++ rules_ui/src/rules_ui/RulesList.tsx
@@ -1,5 +1,8 @@
 import { FragmentType, useFragment } from '../gql/fragment-masking'
 import { graphql } from '../gql'
+import Table from 'react-bootstrap/Table'
+import { useQuery } from '@apollo/client'
+
 
 export const ReviewRuleFragment = graphql(/* GraphQL */ `
   fragment ReviewRuleItem on ReviewRule {
@@ -17,15 +20,48 @@ export const ReviewRuleFragment = graphql(/* GraphQL */ `
   }
 `)
 
-const ReviewRule = (props: {
-    rule: FragmentType<typeof ReviewRuleFragment>
-}) => {
-    const rule = useFragment(ReviewRuleFragment, props.rule)
-    return (
-        <div>
-            <h3>{rule.id}</h3>
-        </div>
+// const ReviewRule = (props: {
+//     rule: FragmentType<typeof ReviewRuleFragment>
+// }) => {
+//     const rule = useFragment(ReviewRuleFragment, props.rule)
+//     return (
+//         <div>
+//             <h3>{rule.id}</h3>
+//         </div>
+//     )
+// }
+
+const allReviewRulesQueryDocument = graphql(/* GraphQL */ `
+  query allReviewRulesQuery {
+    rules {
+      id
+      trigger {
+        triggerName
+      }      
+      ...ReviewRuleItem  
+    }
+  }
+`)
+
+
+const ReviewRuleTable = (props: {}) => {
+    const { data } = useQuery(allReviewRulesQueryDocument)
+    const rules = data?.rules;
+    if (rules == null) {
+        return <div>No rules found</div>;
+    }
+    return (<Table striped bordered hover>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Trigger</th>
+            </tr>
+        </thead>
+        <tbody>
+            {rules.map(e => e && <tr><td>{e.id}</td><td>{e.trigger.triggerName}</td></tr>)}
+        </tbody>
+    </Table>
     )
 }
 
-export default ReviewRule
+export default ReviewRuleTable
